7394f782e0402438dd4f954a3cb9c58012fc78c9
eastend-street <ss_1322irf@icloud.com>
2019-10-08 16:08:20 -0700

    render to html instead of jade

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  // res.render("index", { title: "Express" });
+  res.render('index', { title: 'ejs' });
 });
+
+// exports.index = function(req, res){
+//   res.render('index', { title: 'ejs' });};
 
 module.exports = router;
