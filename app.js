d8424b1606664d7275a842e4ed0c7c6f2e99603c
eastend-street <ss_1322irf@icloud.com>
2019-10-09 16:48:00 -0700

    implemented readAllQuotes function

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
@@ -22,7 +19,46 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://quote-maker-admin:JLlR4Fo3zzoQrc7s@cluster0-kxxb5.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const dbName = "quote_maker";
+
+app.get("/api", (req, res) => {
+  client.connect(err => {
+    const db = client.db(dbName);
+    db.collection("quotes")
+      .find({})
+      .toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        client.close();
+        res.send(result);
+      });
+  });
+});
+
+app.post("/api", (req, res) => {
+  client.connect(err => {
+    const db = client.db(dbName);
+    const document = {
+      quote: "sample4",
+      author: "sample4",
+      canvas_url: "http://i.imgur.com/I86rTVl.jpg",
+      categories: "sample"
+    };
+    db.collection("quotes").insertOne(document, (err, result) => {
+      console.log("Inserted one document into the collection");
+      console.log(err);
+      client.close();
+    });
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,22 +75,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-const MongoClient = require("mongodb").MongoClient;
-const uri =
-  "mongodb+srv://quote-maker-admin:JLlR4Fo3zzoQrc7s@cluster0-kxxb5.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-// const db = client.db("quote-maker");
-// const collection = db.collection("quotes");
-
-// console.log(collection);
 
 module.exports = app;
