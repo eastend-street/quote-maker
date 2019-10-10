f297096080092e54941e766d93d683d0a88eac26
Francois Polo <andasan@gmail.com>
2019-10-09 21:37:50 -0700

    init canvas js script

---
+++ public/script/canvas.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    var canvas = document.getElementById('quoteCanvas');
+    ctx = canvas.getContext('2d');
+
+    // core drawing function
+    var drawQuote = function () {
+
+    };
+
+    //====================
+    // Initialize at startup
+    //==================== 
+    window.setTimeout(function () {
+        drawQuote();
+    }, 100);
+
+});
