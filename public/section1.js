83d0d93d4581de36668a34c57ae87ef38f75aa6b
naoyama88 <naoyama88@gmail.com>
2019-10-11 11:04:32 -0700

    add audio effect sample on next button

--- public/section1.js
+++ public/section1.js
@@ -70,4 +70,22 @@
     }
 
     scrollMenu();
+
+    const playAudio = audio => {
+        audio.play();
+    };
+
+    const audioBeautiful = document.getElementById("audioBeautiful");
+    const nextButton = document.querySelector('#nextButton');
+    nextButton.addEventListener('click', e => {
+        playAudio(audioBeautiful);
+    });
+    nextButton.addEventListener('keydown', e => {
+        const key = e.which || e.keyCode;
+         // 13 is enter
+        if (key !== 13) {
+            return;
+        }
+        playAudio(audioBeautiful);
+    });
 })();
